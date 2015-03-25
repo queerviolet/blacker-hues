683f174fd0bdb15b8a82518c09567dc4db6add7f
Ashi Krishnan <me@ashi.works>
2015-03-25 18:42:22 -0400

    Remove more bb stuff.

--- db/migrate/20150320142359_create_tag_things.rb
+++
@@ -1,9 +0,0 @@
-class CreateTagThings < ActiveRecord::Migration
-  def change
-    create_table :tag_things do |t|
-      t.belongs_to :tag
-      t.belongs_to :thing
-      t.timestamps
-    end
-  end
-end
