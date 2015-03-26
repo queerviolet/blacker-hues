832b5b8b7c5cce2c84f5fba81ff63f4f9cec6d91
Ashi Krishnan <me@ashi.works>
2015-03-26 14:12:23 -0400

    Refactor route names

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,36 +3,36 @@ get '/' do
   redirect "/games/#{game.id}"
 end
 
-get '/games/:id' do
-  game = Game.find(params[:id])
+get '/games/:game_id' do
+  game = Game.find(params[:game_id])
   erb :game, locals: {game: game, tokens: Token.all}
 end
 
 # TODO: DRY this out
-put '/games/:id/player_1_name' do
-  game = Game.find(params[:id])
-  game.player_1 = User.find_or_create_by!(name: params[:player_1_name])
+put '/games/:game_id/player_1_name' do
+  game = Game.find(params[:game_id])
+  game.player_1 = User.find_or_create_by!(name: params[:name])
   game.save!
   redirect back
 end
 
-put '/games/:id/player_2_name' do
-  game = Game.find(params[:id])
-  game.player_2 = User.find_or_create_by!(name: params[:player_2_name])
+put '/games/:game_id/player_2_name' do
+  game = Game.find(params[:game_id])
+  game.player_2 = User.find_or_create_by!(name: params[:name])
   game.save!
   redirect back
 end
 
-put '/games/:id/player_1_throw_id' do
-  game = Game.find(params[:id])
-  game.player_1_throw = Token.find(params[:player_1_throw_id])
+put '/games/:game_id/player_1_throw' do
+  game = Game.find(params[:game_id])
+  game.player_1_throw = Token.find(params[:id])
   game.save!
   redirect back
 end
 
-put '/games/:id/player_2_throw_id' do
-  game = Game.find(params[:id])
-  game.player_2_throw = Token.find(params[:player_2_throw_id])
+put '/games/:game_id/player_2_throw' do
+  game = Game.find(params[:game_id])
+  game.player_2_throw = Token.find(params[:id])
   game.save!
   redirect back
 end
