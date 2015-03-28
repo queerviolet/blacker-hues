5f9de7121f0806a69c76becc6bab6c7f768eefb0
Ashi Krishnan <me@ashi.works>
2015-03-28 16:41:24 -0400

    Style comments and /post/:id get route

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,9 @@
 get '/' do
   erb(:posts, locals: {posts: Post.roots})
 end
+
+get '/posts/:id' do
+  post = Post.find_by(id: params[:id])
+  return 404 unless post
+  erb(:posts, locals: {posts: [post]})
+end
