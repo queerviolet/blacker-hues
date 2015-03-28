7e9db55075df3a59b5c331623a79a0b51e1e565b
Ashi Krishnan <me@ashi.works>
2015-03-28 18:22:12 -0400

    improved usability in posts controller

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -1,44 +1,47 @@
 get '/posts/new' do
-  erb(:'posts/edit', locals: {post: nil})
+  erb(:'posts/edit', locals: {post: nil, parent: nil})
 end
 
 get '/posts/:id' do
-  post = Post.find_by(id: params[:id])
-  return [404, "post not found"] unless post
-  erb(:'posts/index', locals: {posts: [post]})
+  posts = Post.where(id: params[:id])
+  return [404, 'post not found'] unless posts.length > 0
+  erb(:'posts/index', locals: {posts: posts})
 end
 
 get '/posts/:id/edit' do
   post = Post.find_by(id: params[:id])
-  return [404, "post not found"] unless post
+  return [404, 'post not found'] unless post
   erb(:'posts/edit', locals: {post: post, parent: post.parent})
 end
 
-def redirect_to_post_or_parent(post)
-  if post.parent
-    redirect "/posts/#{post.parent.id}"
-  else
-    redirect "/posts/#{post.id}"
+get '/posts/:parent_id/reply' do
+  parent = nil
+  if params.include?(:parent_id)
+    parent = Post.find_by(id: params[:parent_id])
+    return [404, "post not found"] unless parent
   end
+  erb(:'posts/edit', locals: {post: nil, parent: parent})
 end
 
 post '/posts/' do
   parent = nil
   if params[:parent_id]
     parent = Post.find_by(id: params[:parent_id])
-    return [400, "parent post not found"] unless parent
+    return [400, 'parent post not found'] unless parent
   end
   post = Post.new(content: params[:content], user: User.all.sample, parent: parent)
-  return [400, "invalid post"] unless post.valid?
+  return [400, 'invalid post'] unless post.valid?
   post.save!
-  redirect_to_post_or_parent(post)
+  response.headers['X-Created-Post-Id'] = "#{post.id}"
+  response.headers['X-Created-Post-Url'] = "/posts/#{post.id}"
+  redirect back
 end
 
 put '/posts/:id' do
   post = Post.find_by(id: params[:id])
-  return [404, "post not found"] unless post
+  return [404, 'post not found'] unless post
   post.content = params[:content]
-  return [400, "invalid post"] unless post.valid?
+  return [400, 'invalid post'] unless post.valid?
   post.save!
-  redirect_to_post_or_parent(post)
+  redirect back
 end
