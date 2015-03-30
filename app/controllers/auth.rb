696feae544e90ff3c5b90e7ba8d1e17697159dbe
Ashi Krishnan <me@ashi.works>
2015-03-30 09:10:57 -0400

    Auth header cleanup fixing bug where auth is a fixnum. Removed inline post signup.

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,28 +1,27 @@
-
-# This filter logs a user in or creates a new user. It lets us easily
-# inline username / password fields in forms that require login,
-# decreasing friction.
-before do
-  if params[:email] and params[:password]
-    user = User.find_by(email: params[:email])
-    if user
-      return 403 unless user.authenticate(params[:password])
-    else
-      user = User.create!(email: params[:email], password: params[:password])
-    end
-    session[:user_id] = user.id
-  end
-end
-
-def current_user
-  User.find_by(id: session[:user_id])
-end
-
 post '/login' do
+  user = User.find_by(email: params[:email])
+  if user
+    return 403 unless user.authenticate(params[:password])
+  else
+    user = User.create!(email: params[:email], password: params[:password])
+  end
+  session[:user_id] = user.id
   redirect back
 end
 
 post '/logout' do
   session[:user_id] = nil
   redirect back
+end
+
+# This is a sinatra filter, which attaches information about
+# the currently logged in user to every response
+after do
+  if current_user
+    response.headers['X-Current-User-Id'] = "#{current_user.id}"
+  end
+end
+
+def current_user
+  User.find_by(id: session[:user_id])
 end
